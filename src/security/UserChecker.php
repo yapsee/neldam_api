72a418bdbf82f552e25a8157aaeced41dd930b78
Yapsa <yapsasee@gmail.com>
2020-01-09 08:39:59 +0100

    user checker blocage deblocage

---
+++ src/security/UserChecker.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\security;
+ 
+use App\Entity\User as AppUser;
+use Symfony\Component\Security\Core\Exception\AccountExpiredException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+ 
+class UserChecker implements UserCheckerInterface
+{
+    public function checkPreAuth(UserInterface $user)
+    {
+        if (!$user instanceof AppUser) {
+            return;
+        }
+    }
+ 
+    public function checkPostAuth(UserInterface $user)
+    {
+        if (!$user instanceof AppUser) {
+            return;
+        }
+ 
+        // user account is expired, the user may be notified
+        if (!$user->getIsActive()) {
+            throw new AccountExpiredException("ce membre n'est pas actif");
+        }
+    }
+}
